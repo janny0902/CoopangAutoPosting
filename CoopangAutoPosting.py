07b8411bda30f1f10f3219b2bd88cef79e7a900b
janny <wndhks92@daum.net>
2021-04-27 15:35:22 +0900

    [UPD]제품 불러오기 및 이미지 올리기 적용

--- CoopangAutoPosting.py
+++ CoopangAutoPosting.py
@@ -10,17 +10,25 @@ import secrets
 from urllib.parse import urlencode
 from PIL import Image
 from io import BytesIO
+import Sqlite3Conn
+
+from wordpress_xmlrpc import Client
+from wordpress_xmlrpc.methods import posts
+from wordpress_xmlrpc.methods import media
+from wordpress_xmlrpc import WordPressPost
 
 __author__ = "Jaejin Jang<jaejin_me@naver.com>"
 
 class cupangMgr:
 	def __init__(self):
+		
 		self.DOMAIN = "https://api-gateway.coupang.com"
+		
 
-		if not os.path.isdir('imgs'):
-			os.makedirs('imgs')
+		#if not os.path.isdir('imgs'):
+		#	os.makedirs('imgs')
 
-		self.imgpath = os.getcwd() + r"\imgs"
+		#self.imgpath = os.getcwd() + r"\imgs"
 
 		self.options = webdriver.ChromeOptions()
 		self.options.add_argument('headless')
@@ -38,16 +46,19 @@ class cupangMgr:
 		return "CEA algorithm=HmacSHA256, access-key={}, signed-date={}, signature={}".format(accessKey, datetime, signature)
 
 	def get_productsdata(self, request_method, authorization, keyword, limit):
-		URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + urllib.parse.quote(keyword) + "&limit=" + str(limit)
+		#URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + urllib.parse.quote(keyword) + "&limit=" + str(limit)
+		URL = "/v2/providers/affiliate_open_api/apis/openapi/products/bestcategories/1001?limit=" + str(limit)
 		url = "{}{}".format(self.DOMAIN, URL)
 
 		response = requests.request(method=request_method, url=url, headers={ "Authorization": authorization, "Content-Type": "application/json;charset=UTF-8" })
 		retdata = json.dumps(response.json(), indent=4).encode('utf-8')
 		jsondata = json.loads(retdata)
 		data = jsondata['data']
-		productdata = data['productData']
+		print(data)
+		#productdata = data['productData']
 
-		return productdata
+		#return productdata
+		return data
 
 	def make_productcontent(self, each_product):
 		driver = webdriver.Chrome(self.webdriverpath, options=self.options)
@@ -62,38 +73,66 @@ class cupangMgr:
 
 		title = driver.find_element_by_class_name("prod-buy-header__title").text
 		img = driver.find_elements_by_xpath("//*[@class='detail-item']//img")
-
-		images = []
+		content = ''
+		
 
 		for each_img in img:
 			src = each_img.get_attribute('src')
-			if 'vendor_inventory' not in src and ('thumbnail' not in src or 'remote' not in src):
-				continue
-			fname = secrets.token_hex(16)[0:10] + ".png" 
-			fnamefull = self.imgpath + "\\" + fname
-			urllib.request.urlretrieve(src, fnamefull)
-			images.append(('image', (fname, open(fnamefull, 'rb'), 'image/png')))
+			print(src)  ##상품 이미지 url.
+			print("------")
+			content += '<img src = '+src+'>'
 		driver.close()
 
-		return title, producturl, images
+		return title, producturl, content
 
-	
+	def CreatePost(self,title, content, id , password, category):
+		my_blog = Client('http://janny.pe.kr/xmlrpc.php', id, password)
+		myposts=my_blog.call(posts.GetPosts())
+        
+		post = WordPressPost()
+		post.title = title ## 글 제목.
+		post.slug='StockTrade'
+		post.content = content ## 글 내용.       
+        
+        
+        # read the binary file and let the XMLRPC library encode it into base64
+       
+        
+		post.thumbnail = 531  #어드민 페이지 > 미디어 > 이미지에 마우스 올리면 > 왼쪽밑에 url 에 코드 적혀있음   post = ???? << 이게 아이디
+		post.terms_names = {
+                            'post_tag' :[category],
+                            'category': ["쿠팡 제품 소개"] ## 글을 포함시키고 싶은 카테고리를 넣으면된다.
+        }
+		post.id = my_blog.call(posts.NewPost(post))
+		post.post_status = 'publish'
+		my_blog.call(posts.EditPost(post.id, post))   
+
 if __name__ == '__main__':
+	
 	method = 'GET'				 #정보를 얻는것이기 때문에 GET
 	keyword = '노트북' #검색할 키워드, 쿠팡에서 검색하는거랑 결과가 동일합니다.
 	limit = 1					 #몇개의 정보를 가져올지 설정. 상위부터 가져옵니다.
-	access_key = '0e3a5476-d2d7-452b-8c34-09ed35687cc4'			#API access key
-	secret_key = '6e872465260407a2c910010a690de8fe9f0dd948'		#API secret key
-	URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + urllib.parse.quote(keyword) + "&limit=" + str(limit)
+	access_key = '#'			#API access key
+	secret_key = '#'		#API secret key
+	#URL = "/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=" + urllib.parse.quote(keyword) + "&limit=" + str(limit)
+	URL = "/v2/providers/affiliate_open_api/apis/openapi/products/bestcategories/1001?limit=" + str(limit)
+	print(URL)
+#	https://api-gateway.coupang.com/v2/providers/affiliate_open_api/apis/openapi/products/search?keyword=food&limit=50
+#	https://api-gateway.coupang.com/v2/providers/affiliate_open_api/apis/openapi/products/bestcategories/1001?limit=50
+								   
 
 	test = cupangMgr()
 	authorization = test.generateHmac(method, URL, secret_key, access_key)		# HMAC 생성
 	productdata = test.get_productsdata(method, authorization, keyword, limit)	# API 호출
 
 	for each_product in productdata:
-		title, producturl, images = test.make_productcontent(each_product)
+		title, producturl,content = test.make_productcontent(each_product)
 		print('타이틀 : ', title)
 		print('URL : ', producturl)
-		print('이미지파일 : ', images)
+		
+		
+	title = title + ' 제품 소개'
+	category_text = '테스트'
+	test.CreatePost(title, content, 'id#' , 'pass#', category_text)
 
 print('program end')
---
+++ Sqlite3Conn.py
@@ -0,0 +1,196 @@
+import sqlite3
+import time
+
+class SQL_CONNECT:
+    def __init__(self):
+        self.db_path = "C:/Users/janny0902/kiwoom.db"  #DB경로
+        self.nowdate = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+
+    ##--------------------조회 기능--------------------------
+    def SQL_UserSelect(self,tableNm):
+        #회원 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm ) 
+        rows = cur.fetchall()
+        user=[] 
+        
+        for row in rows:      
+            print(row)      
+            user = row
+        conn.close()
+        return user
+    def SQL_Count(self,tableNm):
+        #회원 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM " + tableNm ) 
+        rows = cur.fetchall()
+        
+        conn.close()
+        return rows
+
+    def SQL_ListSelect(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm ) 
+        rows = cur.fetchall()        
+        item = []        
+        count=0
+        for row in rows:              
+            item.insert(count,row[0]) 
+            count+=1
+        conn.close()
+        
+    def SQL_ListSeach(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm ) 
+        rows = cur.fetchall()        
+        item = []        
+        count=0
+        for row in rows:              
+            item.insert(count,row) 
+            count+=1
+        conn.close()
+        return(item)
+        
+    def SQL_ListReport(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm+" WHERE S_DT = ?", [self.nowdate]) 
+        rows = cur.fetchall()        
+        item = []        
+        count=0
+        for row in rows:              
+            item.insert(count,row) 
+            count+=1
+        conn.close()
+        return(item)
+
+
+    def SQL_ItemSelect(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm +" WHERE USE_YN = 'Y'") 
+        rows = cur.fetchall()            
+        
+        count=0
+        return rows
+
+    def SQL_ItemList(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm +" WHERE USE_YN = 'N'") 
+        rows = cur.fetchall()       
+        count=0      
+        conn.close()
+        
+        return(rows)
+    def SQL_ItemListO(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm +" WHERE USE_YN = 'O'") 
+        rows = cur.fetchall()       
+        count=0      
+        conn.close()
+        
+        return(rows)
+
+    def SQL_MsgSelect(self,tableNm):
+        #테이블 조회하기
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm ) 
+        rows = cur.fetchall()
+        print(rows)
+
+        item = []
+        count=0
+        for row in rows:                  
+            item.insert(count,row[0]) 
+            count+=1
+        conn.close()
+        
+        return item
+
+        
+    def Sql_Distinct(self,tableNm,data):
+        #데이터 중복값 있는지 조회
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM " + tableNm +' WHERE S_NUM = ?' , data ) 
+        rows = cur.fetchall()
+
+        return rows
+        
+
+    ## ---------------------- 삽입기능(INSERT)----------------------------
+    def SQL_Insert_f(self,SqlString,s_tuple,useYn,n_Name,d_Date): 
+        #데이터 삽입하기    
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(SqlString,(s_tuple,useYn,n_Name,d_Date)) 
+        conn.commit()
+        conn.close()
+
+    def SQL_Insert(self,SqlString,s_tuple):
+        #데이터 삽입하기    
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(SqlString,(s_tuple) )
+        conn.commit()
+        conn.close()
+    
+
+    ## ---------------------- 수정기능(UPDATE) ---------------------------
+    def SQL_Update(self,SqlString,data):
+        #데이터 업데이트        
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(SqlString,data) 
+        conn.commit()
+        conn.close()
+
+   
+    ## ---------------------- 삭제기능(DELETE) ---------------------------
+    def SQL_Delete(self,tableName):
+        #데이터 업데이트        
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute("DELETE FROM " + tableName) 
+        conn.commit()
+        conn.close()
+
+    def SQL_DeleteW(self,sqlString,data):
+        #데이터 업데이트        
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(sqlString,data) 
+        conn.commit()
+        conn.close()
+
+    def SQL_DeleteB(self,sqlString):
+        #데이터 업데이트        
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(sqlString) 
+        conn.commit()
+        conn.close()
+
+    def SQL_DeleteWD(self,sqlString):
+        #데이터 업데이트        
+        conn = sqlite3.connect(self.db_path)
+        cur = conn.cursor()
+        cur.execute(sqlString) 
+        conn.commit()
+        conn.close()
+
+    
+    
